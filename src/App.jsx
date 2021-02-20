5759a4b751535c4691c3e6400372bfc1f5865415
Mario Olivera <mario.olivera96@gmail.com>
2021-02-20 09:44:35 -0300

    principal routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import React from 'react'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
+    <Router>
+      <Switch>
 
-    </div>
+        <Route path="/experiencie" exact>
+          <div>/experiencie</div>
+        </Route>
+
+        <Route path="/education" exact>
+          <div>/education</div>
+        </Route>
+
+        <Route path="/skills" exact>
+          <div>/skills</div>
+        </Route>
+
+        <Route path="/interest" exact>
+          <div>/interest</div>
+        </Route>
+
+        <Route path="/interests" exact>
+          <div>/interests</div>
+        </Route>
+
+        <Route path="/awards">
+          <div>/awards</div>
+        </Route>
+
+        <Route path="/">
+          <div>ABOUT/HOME</div>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
